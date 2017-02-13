d4adc5a4f882dc08a08eddd10d061317bae3ff8f
admin <al-not@ya.ru>
2017-02-14 00:15:33 +0500

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -4,64 +4,62 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /*
 Составить цепочку слов
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
+        Long t0 = System.currentTimeMillis();
         try (
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 BufferedReader readFile = new BufferedReader(new FileReader("c:\\data.txt"))
         ) {
-            StringBuilder sb = new StringBuilder();
+            String[] words = new String[]{};
 
             while (readFile.ready())
-                sb.append(readFile.readLine() + " ");
-
-            String[] words = sb.toString().split("\\s+");
+                words = readFile.readLine().split("\\s+");
 
             StringBuilder result = getLine(words);
             System.out.println(result.toString());
         }
+        Long t1 = System.currentTimeMillis();
+        System.out.println("Time: " + (t1 - t0)/ 1000d + " sec");
+        System.out.println("Memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024) + " mb");
     }
 
     public static StringBuilder getLine(String... words) {
         StringBuilder sb = new StringBuilder();
+        int count = 0;
         String lastSymbol = "";
-        String[] tmpWords;
 
-        if (words.length > 0 && !words[0].equals("")) {
-            tmpWords = words;
-            Arrays.sort(tmpWords);
+        if (words != null && words.length == 1) {
+            sb.append(words[0]);
 
-            for (int m = 0; m < tmpWords.length; m++) {
-                for (int n = 0; n < tmpWords.length; n++) {
-                    if (tmpWords[m].equals(tmpWords[n]) && m != n)
-                        tmpWords[n] = "";
+        } else if (words != null && words.length > 0) {
+            List<String> strArr = new ArrayList<>(Arrays.asList(words));
+
+            while (count != strArr.size()) {
+                count = 0;
+                sb = new StringBuilder();
+                sb.append(strArr.get(0));
+
+                for (int i = 1; i < strArr.size(); i++) {
+                    lastSymbol = String.valueOf(strArr.get(i - 1).charAt(strArr.get(i - 1).length() - 1)).toUpperCase();
+
+                    if (strArr.get(i).startsWith(lastSymbol)) {
+                        sb.append(" " + strArr.get(i));
+                        count++;
+                    } else break;
+                    System.out.println(strArr.size() + " " + count);
                 }
-            }
-
-            for (int i = 0; i < tmpWords.length; i++) {
-
-                if (sb.length() == 0) {
-                    sb.append(tmpWords[i]);
-                    lastSymbol = String.valueOf(tmpWords[i].charAt(tmpWords[i].length() - 1)).toUpperCase();
-                    tmpWords[i] = "";
-                }
-
-                for (int j = 0; j < tmpWords.length; j++) {
-
-                    if (i != j && tmpWords[j].toUpperCase().startsWith(lastSymbol)) {
-                        sb.append(" " + tmpWords[j]);
-                        lastSymbol = String.valueOf(tmpWords[j].charAt(tmpWords[j].length() - 1)).toUpperCase();
-                        tmpWords[j] = "";
-                    }
-                }
+                Collections.shuffle(strArr);
             }
         }
-
         return sb;
     }
 }
