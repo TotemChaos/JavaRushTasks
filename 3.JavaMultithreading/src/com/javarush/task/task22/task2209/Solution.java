86fd1d1423785c93189a9febe322451afa0367d9
zolkin <al-not@ya.ru>
2017-02-14 16:47:28 +0500

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /*
 Составить цепочку слов
@@ -19,47 +16,44 @@ public class Solution {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 BufferedReader readFile = new BufferedReader(new FileReader("c:\\data.txt"))
         ) {
-            String[] words = new String[]{};
+            StringBuilder sb = new StringBuilder();
 
             while (readFile.ready())
-                words = readFile.readLine().split("\\s+");
+                    sb.append(readFile.readLine() + " ");
+
+            String[] words = sb.toString().split("\\s+");
 
             StringBuilder result = getLine(words);
-            System.out.println(result.toString());
+            System.out.println(result.toString().trim());
         }
         Long t1 = System.currentTimeMillis();
-        System.out.println("Time: " + (t1 - t0)/ 1000d + " sec");
+        System.out.println("Time: " + (t1 - t0) / 1000d + " sec");
         System.out.println("Memory: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024) + " mb");
     }
 
     public static StringBuilder getLine(String... words) {
         StringBuilder sb = new StringBuilder();
+
+        if (words == null || words.length == 0) return sb;
+        else if (words.length == 1) return sb.append(words[0]);
+
+        List<String> strArr = new ArrayList<>(Arrays.asList(words));
         int count = 0;
-        String lastSymbol = "";
 
-        if (words != null && words.length == 1) {
-            sb.append(words[0]);
+        while (count != strArr.size() - 1) {
+            Collections.shuffle(strArr);
+            count = 0;
 
-        } else if (words != null && words.length > 0) {
-            List<String> strArr = new ArrayList<>(Arrays.asList(words));
-
-            while (count != strArr.size()) {
-                count = 0;
-                sb = new StringBuilder();
-                sb.append(strArr.get(0));
-
-                for (int i = 1; i < strArr.size(); i++) {
-                    lastSymbol = String.valueOf(strArr.get(i - 1).charAt(strArr.get(i - 1).length() - 1)).toUpperCase();
-
-                    if (strArr.get(i).startsWith(lastSymbol)) {
-                        sb.append(" " + strArr.get(i));
-                        count++;
-                    } else break;
-                    System.out.println(strArr.size() + " " + count);
-                }
-                Collections.shuffle(strArr);
+            for (int i = 0; i < strArr.size() - 1; i++) {
+                if (strArr.get(i).endsWith(String.valueOf(strArr.get(i + 1).charAt(0)).toLowerCase())) {
+                    count++;
+                } else break;
             }
         }
+
+        for (String s : strArr)
+            sb.append(s + " ");
+
         return sb;
     }
 }
