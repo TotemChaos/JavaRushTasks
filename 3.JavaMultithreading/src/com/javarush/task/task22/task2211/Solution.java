012a5bbe6ded429a71d408171eb545cec2d0d1ec
zolkin <al-not@ya.ru>
2017-02-16 15:15:21 +0500

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task22.task2211;
 
-import java.io.IOException;
+import java.io.*;
 
 /* 
 Смена кодировки
@@ -9,6 +9,16 @@ public class Solution {
     static String win1251TestString = "РќР°СЂСѓС€РµРЅРёРµ РєРѕРґРёСЂРѕРІРєРё РєРѕРЅСЃРѕР»Рё?"; //only for your testing
 
     public static void main(String[] args) throws IOException {
-
+        try(
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(args[0]));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(args[1]))
+        )
+        {
+            byte[] buffer = new byte[bis.available()];
+            bis.read(buffer);
+            String s = new String(buffer, "UTF-8");
+            buffer = s.getBytes("Windows-1251");
+            bos.write(buffer);
+        }
     }
 }
