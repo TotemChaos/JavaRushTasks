cf15a3e035275d97f78885144b259d1a37beac8b
zolkin <al-not@ya.ru>
2017-02-17 12:50:06 +0500

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -1,12 +1,82 @@
 package com.javarush.task.task22.task2207;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /* 
 Обращенные слова
 */
+public class Solution {
+    public static List<Pair> result = new LinkedList<>();
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader readFile = new BufferedReader(new FileReader(reader.readLine()))
+        ) {
+            List<String> tmpArr = new ArrayList<>();
+
+            while (readFile.ready()) {
+                tmpArr.addAll(Arrays.asList(readFile.readLine().split("\\s+")));
+            }
+
+            for (int i = 0; i < tmpArr.size(); ) {
+                StringBuilder reverse = new StringBuilder(tmpArr.get(i)).reverse();
+                tmpArr.remove(i);
+                for (int j = 0; j < tmpArr.size(); j++) {
+                    if (reverse.toString().equals(tmpArr.get(j)) && !reverse.toString().equals("")) {
+                        Pair p = new Pair();
+                        p.first = reverse.reverse().toString();
+                        p.second = tmpArr.get(j);
+                        result.add(p);
+                        tmpArr.remove(j);
+                        break;
+                    }
+                }
+            }
+            for (Pair pair : result)
+                System.out.println(pair);
+        }
+    }
+
+    public static class Pair {
+        String first;
+        String second;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return first == null && second == null ? "" :
+                    first == null && second != null ? second :
+                            second == null && first != null ? first :
+                                    first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
+
+        }
+    }
+}
+/*
+package com.javarush.task.task22.task2207;
+
+import java.io.*;
+import java.util.*;
+
 public class Solution {
     public static List<Pair> result = new LinkedList<>();
 
@@ -82,3 +152,4 @@ public class Solution {
     }
 
 }
+*/
