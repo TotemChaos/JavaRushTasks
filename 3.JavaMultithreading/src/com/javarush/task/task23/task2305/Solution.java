5a69c2ca7e64cb34795b9e6f070c31bd70b39662
zolkin <al-not@ya.ru>
2017-02-20 16:34:50 +0500

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -10,8 +10,15 @@ public class Solution {
     }
 
     public static Solution[] getTwoSolutions() {
+        Solution solution1 = new Solution();
+        Solution solution2 = new Solution();
 
-        return null;
+        solution1.innerClasses = new InnerClass[2]{new Solution().new InnerClass(), new Solution().new InnerClass()};
+        solution2.innerClasses = new InnerClass[2];
+
+        Solution[] sol = new Solution[]{solution1, solution2};
+
+        return sol;
     }
 
     public static void main(String[] args) {
